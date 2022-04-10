270f9400852c2afe8ad97fff6504599e993851f6
Nyckzin10 <backtonarnia@hotmail.com>
2022-04-10 18:19:34 -0300

    Continuação

--- programa03/main.cpp
+++ programa03/main.cpp
@@ -1,16 +1,22 @@
-#include <stdint.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-float vat[20], soma=0, media; int posicao;
-
-
-for (posicao=0;posicao=<20;posicao++){
-    scanf("%f", &vet[posicao]);
-    soma=soma+vet[posicao]
-}
-media=soma/20;
-for (posicao=0;posicao=<20;posicao++)
+int main ()
 {
-    if(vet[posicao] >= media)
-    printf("%.2f \m", vet[posicao]);
+    const int target=5;
+    float vat[target] soma=0,media; int posicao;
+    for (posicao=0;posicao<target;posicao++)
+    {
+        scanf("%f", &vet[posicao]);
+        soma = soma+vet[posicao];
+
+    }
+    media = soma/target;
+    for (posicao=0;posicao<target;posicao++)
+    {
+        if (vet[posicao]!>=media)
+            printf("%2.f", vet[posicao]);
+
+    }
+    return 0;
 }
+
